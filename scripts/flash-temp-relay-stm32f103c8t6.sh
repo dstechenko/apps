25e35799535ee49342a656243ec7230950711db3
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2024-10-03 06:58:42 -0400

    temp-relay: add cpuid override

--- scripts/flash-temp-relay-stm32f103c8t6.sh
+++ scripts/flash-temp-relay-stm32f103c8t6.sh
@@ -3,3 +3,5 @@ openocd                                         \
   -f ../../../apps/configs/interface/stlink.cfg \
   -f ../../../apps/configs/target/stm32f1x.cfg  \
   -c "program Build/temp-relay-stm32f103c8t6.hex verify reset exit"
+
+# -c "set CPUTAPID 0x2ba01477"

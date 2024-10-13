b67db18d58447b6c8d25f22722b5dcf682bca34e
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2024-10-13 09:39:54 -0400

    temp-relay: add temp reads/writes

--- temp-relay/src/main.c
+++ temp-relay/src/main.c
@@ -9,7 +9,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/util.h>
 
-#define SLEEP_TIME_MS 2000
+#define SLEEP_TIME_MS 1000
 
 static const struct gpio_dt_spec led_alive = GPIO_DT_SPEC_GET(DT_ALIAS(led_alive), gpios);
 static const struct i2c_dt_spec i2c_temp = I2C_DT_SPEC_GET(DT_ALIAS(i2c_temp));
@@ -25,14 +25,12 @@ static const struct i2c_dt_spec i2c_temp = I2C_DT_SPEC_GET(DT_ALIAS(i2c_temp));
 #define CONFIG_MONITOR_STACK_SIZE 500
 #define CONFIG_MONITOR_PRIORITY 5
 
-static const uint8_t TMP102_ADDR_READ = 0x48 << 1;
-static const uint8_t TMP102_ADDR_WRITE = TMP102_ADDR_READ | 0x01;
-static const uint8_t TMP102_REG_TCUR = 0x00;
+static const uint8_t TMP102_REG_CURR = 0x00;
 static const uint8_t TMP102_REG_CONF = 0x01;
-static const uint8_t TMP102_REG_TLOW = 0x02;
-static const uint8_t TMP102_REG_THIGH = 0x03;
+static const uint8_t TMP102_REG_LOW = 0x02;
+static const uint8_t TMP102_REG_HIGH = 0x03;
 
-extern float temp_from_bits(const uint8_t msb, const uint8_t lsb)
+static float temp_from_bits(const uint8_t msb, const uint8_t lsb)
 {
 	int16_t val = ((int16_t)msb << 4) | (lsb >> 4);
 	if (val > 0x7FF) {
@@ -41,28 +39,81 @@ extern float temp_from_bits(const uint8_t msb, const uint8_t lsb)
 	return (val * 0.0625) * 100;
 }
 
-extern uint16_t temp_to_bits(const float temp)
+static int16_t temp_to_bits(const float temp)
 {
-	int16_t bits = (int16_t)(temp / 0.0625);
+	int16_t bits = (int16_t)((double)temp / 0.0625);
 	if (bits < 0) {
 		bits &= 0x0FFF;
 	}
 	return bits;
 }
 
-extern int temp_read_config(uint16_t *config)
+static int temp_read_config(uint16_t *config)
 {
 	int err;
-	uint8_t buf[2] = { TMP102_REG_CONF, 0 };
+	uint8_t buf[2];
 
 	if (!i2c_is_ready_dt(&i2c_temp)) {
 		return -ENODEV;
 	}
 
+	buf[0] = TMP102_REG_CONF;
+	buf[1] = 0;
+
 	err = i2c_write_read_dt(&i2c_temp, buf, 1, buf, sizeof(buf));
 	*config = (buf[0] << 8) | buf[1];
 
 	return err;
+}
+
+static int temp_write_config(const uint16_t config)
+{
+	uint8_t buf[3];
+
+	if (!i2c_is_ready_dt(&i2c_temp)) {
+		return -ENODEV;
+	}
+
+	buf[0] = TMP102_REG_CONF;
+	buf[1] = config >> 8;
+	buf[2] = config & 0xFF;
+
+	return i2c_write_dt(&i2c_temp, buf, sizeof(buf));
+}
+
+static int temp_read(const uint8_t reg, float *temp)
+{
+	int err;
+	uint8_t buf[2];
+
+	if (!i2c_is_ready_dt(&i2c_temp)) {
+		return -ENODEV;
+	}
+
+	buf[0] = reg;
+	buf[1] = 0;
+
+	err = i2c_write_read_dt(&i2c_temp, buf, 1, buf, sizeof(buf));
+	*temp = temp_from_bits(buf[0], buf[1]);
+
+	return err;
+}
+
+static int temp_write(const uint8_t reg, const float temp)
+{
+	uint8_t buf[3];
+	int16_t temp_bits;
+
+	if (!i2c_is_ready_dt(&i2c_temp)) {
+		return -ENODEV;
+	}
+
+	temp_bits = temp_to_bits(temp);
+	buf[0] = reg;
+	buf[1] = (uint8_t)(temp_bits >> 4);
+	buf[2] = (uint8_t)(temp_bits << 4);
+
+	return i2c_write_dt(&i2c_temp, buf, sizeof(buf));
 }
 
 static void comms_run_thread()
@@ -112,7 +163,6 @@ static void info_init_timer(void)
 static void info_run_thread()
 {
 	int sig_state, sig_result;
-	bool led_state = true;
 
 	info_init_gpio();
 	info_init_signals();
@@ -129,12 +179,6 @@ static void info_run_thread()
 
 		if (sig_state && sig_result == INFO_ALIVE_SIGNAL) {
 			gpio_pin_toggle_dt(&led_alive);
-			led_state = !led_state;
-			printf("LED state: %s\n", led_state ? "ON" : "OFF");
-			uint16_t config = 0;
-			if (!temp_read_config(&config)) {
-				printf("Config: 0x%x\n", config);
-			}
 		}
 	}
 }
@@ -143,7 +187,38 @@ K_THREAD_DEFINE(info_thread, CONFIG_INFO_STACK_SIZE, info_run_thread, NULL, NULL
 
 static void monitor_run_thread()
 {
+	uint16_t config = 0;
+	float temp = 0;
+
+	if (!temp_read(TMP102_REG_LOW, &temp)) {
+		printf("Low on init: %u.%02u C\n", ((unsigned int)temp / 100), ((unsigned int)temp % 100));
+	}
+	temp_write(TMP102_REG_LOW, 29.0);
+	if (!temp_read(TMP102_REG_LOW, &temp)) {
+		printf("Low now: %u.%02u C\n", ((unsigned int)temp / 100), ((unsigned int)temp % 100));
+	}
+	if (!temp_read(TMP102_REG_HIGH, &temp)) {
+		printf("High on init: %u.%02u C\n", ((unsigned int)temp / 100), ((unsigned int)temp % 100));
+	}
+	temp_write(TMP102_REG_HIGH, 31.0);
+	if (!temp_read(TMP102_REG_HIGH, &temp)) {
+		printf("High now: %u.%02u C\n", ((unsigned int)temp / 100), ((unsigned int)temp % 100));
+	}
+
+	if (!temp_read_config(&config)) {
+		printf("Config on init: 0x%x\n", config);
+	}
+	if (!temp_write_config(config | (1 << 10))) {
+		printf("Config updated...\n");
+	}
+	if (!temp_read_config(&config)) {
+		printf("Config now: 0x%x\n", config);
+	}
+
 	while (true) {
+		if (!temp_read(TMP102_REG_CURR, &temp)) {
+			printf("Current: %u.%02u C\n", ((unsigned int)temp / 100), ((unsigned int)temp % 100));
+		}
 		k_msleep(SLEEP_TIME_MS);
 	}
 }

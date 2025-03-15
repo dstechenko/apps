5e5786e8a6119fcaa2db9dbf935b282017b2bb82
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2025-03-15 10:34:49 -0400

    keebz: add usb dev setup bits

--- keebz/src/keebz_usb_dev.c
+++ keebz/src/keebz_usb_dev.c
@@ -9,10 +9,25 @@
 
 LOG_MODULE_REGISTER(keebz_usb_dev, CONFIG_KEEBZ_USB_DEV_LOG_LEVEL);
 
+USBD_DEVICE_DEFINE(keebz_usb_dev, DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)), CONFIG_KEEBZ_USB_DEV_VENDOR_ID,
+		   CONFIG_KEEBZ_USB_DEV_PRODUCT_ID);
+
 struct usbd_context *keebz_usb_dev_init(usbd_msg_cb_t cb)
 {
-	(void)cb;
-	return NULL;
+	int err;
+
+	if (!keebz_usb_dev_setup(cb)) {
+		LOG_ERR("Failed to setup device");
+		return NULL;
+	}
+
+	err = usbd_init(&keebz_usb_dev);
+	if (err) {
+		LOG_ERR("Failed to initialize device support, err=%d", err);
+		return NULL;
+	}
+
+	return &keebz_usb_dev;
 }
 
 struct usbd_context *keebz_usb_dev_setup(usbd_msg_cb_t cb)
